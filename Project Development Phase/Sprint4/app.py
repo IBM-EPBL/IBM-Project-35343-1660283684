9fd39b3ec2e3e6dff62e6101b0795bb5107f58cc
nandhu-6 <114221757+nandhu-6@users.noreply.github.com>
2022-11-08 14:01:28 +0530

    Update app.py

--- Project Development Phase/Sprint4/app.py
+++ Project Development Phase/Sprint4/app.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, request, redirect, session 
-from flask_mysqldb import MySQL
-import mysql.connector
-import MySQLdb.cursors
+import ibm_db
 import re
-app = Flask(__name__)
-app.secret_key = 'a'  
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'D2DxDUPBii'
-mysql = MySQL(app)
 
+app = Flask(__name__)
+
+
+app.secret_key = 'a'
+  
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30699;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=mbs46040;PWD=MIEpZ1DoqwMRpGvs",'','')
+
+#HOME--PAGE
 @app.route("/home")
 def home():
     return render_template("homepage.html")
+
 @app.route("/")
 def add():
     return render_template("home.html")
+  
+
+
+#SIGN--UP--OR--REGISTER
+
 
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
+
+
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
@@ -28,6 +36,8 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        
+
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM register WHERE username = % s', (username, ))
         account = cursor.fetchone()
@@ -43,41 +53,66 @@ def register():
             mysql.connection.commit()
             msg = 'You have successfully registered !'
             return render_template('signup.html', msg = msg)
+        
+        
+ 
+        
+ #LOGIN--PAGE
     
 @app.route("/signin")
 def signin():
-    return render_template("login.html")       
+    return render_template("login.html")
+        
 @app.route('/login',methods =['GET', 'POST'])
 def login():
     global userid
-    msg = ''     
+    msg = ''
+   
+  
     if request.method == 'POST' :
         username = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM register WHERE username = % s AND password = % s', (username, password ),)
         account = cursor.fetchone()
-        print (account)       
+        print (account)
+        
         if account:
             session['loggedin'] = True
             session['id'] = account[0]
             userid=  account[0]
-            session['username'] = account[1]           
+            session['username'] = account[1]
+           
             return redirect('/home')
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
 
+
+
+       
+
+
+
+
+
+#ADDING----DATA
+
+
 @app.route("/add")
 def adding():
     return render_template('add.html')
+
+
 @app.route('/addexpense',methods=['GET', 'POST'])
 def addexpense():
+     
     date = request.form['date']
     expensename = request.form['expensename']
     amount = request.form['amount']
     paymode = request.form['paymode']
-    category = request.form['category']    
+    category = request.form['category']
+    
     cursor = mysql.connection.cursor()
     cursor.execute('INSERT INTO expenses VALUES ( % s, % s, % s, % s, % s, % s)', (session['id'] ,date, expensename, amount, paymode, category))
     mysql.connection.commit()
@@ -85,38 +120,81 @@ def addexpense():
     
     return redirect("/display")
 
+
+
+
+
+#DISPLAY---graph 
+
 @app.route("/display")
 def display():
     print(session["username"],session['id'])
+    
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM expenses ORDER BY date DESC'.format(str(session['id'])))
-    expense = cursor.fetchall()       
-    return render_template('display.html' ,expense = expense)   
+    expense = cursor.fetchall()
+  
+       
+    return render_template('display.html' ,expense = expense)
+                          
+                          
+
+
+
+#delete---the--data
+
+@app.route('/delete/<string:id>', methods = ['POST', 'GET' ])
+def delete(id):
+     cursor = mysql.connection.cursor()
+     cursor.execute('DELETE FROM expenses WHERE  userid = %s', (id,))
+     mysql.connection.commit()
+     print('deleted successfully')    
+     return redirect("/display")
+ 
+    
+#UPDATE---DATA
 
 @app.route('/edit/<id>', methods = ['POST', 'GET' ])
 def edit(id):
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT * FROM expenses WHERE  userid = %s', (id,))
-    row = cursor.fetchall()  
+    row = cursor.fetchall()
+   
     print(row[0])
     return render_template('edit.html', expenses = row[0])
+
+
+
+
 @app.route('/update/<id>', methods = ['POST'])
 def update(id):
-  if request.method == 'POST' :   
+  if request.method == 'POST' :
+   
       date = request.form['date']
       expensename = request.form['expensename']
       amount = request.form['amount']
       paymode = request.form['paymode']
-      category = request.form['category']   
-      cursor = mysql.connection.cursor()       
+      category = request.form['category']
+    
+      cursor = mysql.connection.cursor()
+       
       cursor.execute("UPDATE `expenses` SET `date` = % s , `expensename` = % s , `amount` = % s, `paymode` = % s, `category` = % s WHERE `expenses`.`userid` = % s ",(date, expensename, amount, str(paymode), str(category),id))
       mysql.connection.commit()
       print('successfully updated')
-      return redirect("/display")            
+      return redirect("/display")
+     
+      
 
+            
+ 
+         
+    
+            
+ #limit
 @app.route("/limit" )
 def limit():
        return redirect('/limitn')
+
 @app.route("/limitnum" , methods = ['POST' ])
 def limitnum():
      if request.method == "POST":
@@ -124,14 +202,20 @@ def limitnum():
          cursor = mysql.connection.cursor()
          cursor.execute('INSERT INTO limits VALUES (% s, % s) ',(session['id'], number))
          mysql.connection.commit()
-         return redirect('/limitn')         
+         return redirect('/limitn')
+     
+         
 @app.route("/limitn") 
 def limitn():
     cursor = mysql.connection.cursor()
     cursor.execute('SELECT limits FROM limits ORDER BY `limits`.`id` DESC LIMIT 1')
     x= cursor.fetchone()
-    s = x#[0]    
+    s = x#[0]
+    
+    
     return render_template("limit.html" , y= s)
+
+#REPORT
 
 @app.route("/today")
 def today():
@@ -176,18 +260,146 @@ def today():
               t_EMI  += x[4]
          
           elif x[6] == "other":
-              t_other  += x[4]     
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+
+     
+      return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
+                           t_food = t_food,t_entertainment =  t_entertainment,
+                           t_business = t_business,  t_rent =  t_rent, 
+                           t_EMI =  t_EMI,  t_other =  t_other )
+     
+
+@app.route("/month")
+def month():
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT DATE(date), SUM(amount) FROM expenses WHERE userid= %s AND MONTH(DATE(date))= MONTH(now()) GROUP BY DATE(date) ORDER BY DATE(date) ',(str(session['id'])))
+      texpense = cursor.fetchall()
+      print(texpense)
+      
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT * FROM expenses WHERE userid = % s AND MONTH(DATE(date))= MONTH(now()) AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+      expense = cursor.fetchall()
+  
+      total=0
+      t_food=0
+      t_entertainment=0
+      t_business=0
+      t_rent=0
+      t_EMI=0
+      t_other=0
+ 
+     
+      for x in expense:
+          total += x[4]
+          if x[6] == "food":
+              t_food += x[4]
+            
+          elif x[6] == "entertainment":
+              t_entertainment  += x[4]
+        
+          elif x[6] == "business":
+              t_business  += x[4]
+          elif x[6] == "rent":
+              t_rent  += x[4]
+           
+          elif x[6] == "EMI":
+              t_EMI  += x[4]
+         
+          elif x[6] == "other":
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+
+     
+      return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
+                           t_food = t_food,t_entertainment =  t_entertainment,
+                           t_business = t_business,  t_rent =  t_rent, 
+                           t_EMI =  t_EMI,  t_other =  t_other )
+         
+@app.route("/year")
+def year():
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT MONTH(date), SUM(amount) FROM expenses WHERE userid= %s AND YEAR(DATE(date))= YEAR(now()) GROUP BY MONTH(date) ORDER BY MONTH(date) ',(str(session['id'])))
+      texpense = cursor.fetchall()
+      print(texpense)
+      
+      cursor = mysql.connection.cursor()
+      cursor.execute('SELECT * FROM expenses WHERE userid = % s AND YEAR(DATE(date))= YEAR(now()) AND date ORDER BY `expenses`.`date` DESC',(str(session['id'])))
+      expense = cursor.fetchall()
+  
+      total=0
+      t_food=0
+      t_entertainment=0
+      t_business=0
+      t_rent=0
+      t_EMI=0
+      t_other=0
+ 
+     
+      for x in expense:
+          total += x[4]
+          if x[6] == "food":
+              t_food += x[4]
+            
+          elif x[6] == "entertainment":
+              t_entertainment  += x[4]
+        
+          elif x[6] == "business":
+              t_business  += x[4]
+          elif x[6] == "rent":
+              t_rent  += x[4]
+           
+          elif x[6] == "EMI":
+              t_EMI  += x[4]
+         
+          elif x[6] == "other":
+              t_other  += x[4]
+            
+      print(total)
+        
+      print(t_food)
+      print(t_entertainment)
+      print(t_business)
+      print(t_rent)
+      print(t_EMI)
+      print(t_other)
+
+
+     
       return render_template("today.html", texpense = texpense, expense = expense,  total = total ,
                            t_food = t_food,t_entertainment =  t_entertainment,
                            t_business = t_business,  t_rent =  t_rent, 
                            t_EMI =  t_EMI,  t_other =  t_other )
 
 #log-out
+
 @app.route('/logout')
+
 def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    return render_template('home.html')
+
+             
+
 if __name__ == "__main__":
     app.run(debug=True)
